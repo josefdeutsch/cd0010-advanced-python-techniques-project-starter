b6a81db2dde954858651332b3d020ed30d0a636f
Joseph <philippdeutsch@icloud.com>
2024-04-18 19:42:15 +0000

    extract.py implementation of load_approaches()

--- extract.py
+++ extract.py
@@ -47,17 +47,39 @@ def load_neos(neo_csv_path):
     return neos
 
     
-neos = load_neos('/Users/Joseph/Documents/capstone/cd0010-advanced-python-techniques-project-starter/data/neos.csv')
-print(neos)  # to see the output
+#neos = load_neos('/Users/Joseph/Documents/capstone/cd0010-advanced-python-techniques-project-starter/data/neos.csv')
+#print(neos)  # to see the output
 
 
 
 def load_approaches(cad_json_path):
-    """Read close approach data from a JSON file.
-
-    :param cad_json_path: A path to a JSON file containing data about close approaches.
-    :return: A collection of `CloseApproach`es.
-    """
-    # TODO: Load close approach data from the given JSON file.
     
-    return ()
+    """
+    Load and process JSON data into a list of CloseApproach objects.
+
+    Args:
+    filepath (str): The path to the JSON file containing close approach data.
+
+    Returns:
+    list: A list of CloseApproach objects initialized with data from the file.
+    """
+
+    with open(cad_json_path, 'r') as file:
+        data = json.load(file)
+
+    # Extract data items
+    close_approaches = []
+    for entry in data['data']:
+        des = entry[0]
+        cd = entry[3]
+        dist = float(entry[4])
+        v_rel = float(entry[7])
+
+        # Create a CloseApproach object and append it to the list
+        approach = CloseApproach(designation=des, time=cd, distance=dist, velocity=v_rel)
+        close_approaches.append(approach)
+
+    return close_approaches
+
+#close_approaches = load_approaches('/Users/Joseph/Documents/capstone/cd0010-advanced-python-techniques-project-starter/data/cad.json')
+#print(close_approaches)
