ee30d7834ddfdf1622bf9e156c2fee1b1dd7838b
Joseph <philippdeutsch@icloud.com>
2024-04-18 14:21:33 +0000

    models.py - class implementation CloseApproach [constructor, property, representation]

---
+++ data/CloseApproachDefaultData.py
@@ -0,0 +1,7 @@
+# ca = CloseApproach(
+#     designation=170903,  # Integer 
+#     time="1900-Jan-01 00:11", # String
+#     distance=0.0921795123769547,  # Float 
+#     velocity=16.7523040362574,  # Float 
+#     neo=None)
+# 
---
+++ data/NearEarthObjectDefaultData.py
@@ -0,0 +1,7 @@
+# neo_eros = NearEarthObject(
+#     designation="433",
+#     name="Eros",
+#     diameter=float("16.84"),  # Convert string to float
+#     hazardous=False  # since "N" means not hazardous
+# )
+# 
